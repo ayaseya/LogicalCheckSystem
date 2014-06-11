27d0a68b5569697d60cf332614b1753161413118
1218AND <1218AND@192.168.1.177>
2014-06-11 12:00:26 +0900

    登録画面のひな型が完成

--- src/com/example/logicalchecksystem/Customer.java
+++ src/com/example/logicalchecksystem/Customer.java
@@ -4,9 +4,9 @@ package com.example.logicalchecksystem;
 public class Customer {
 
 	private String _id;// 顧客に割り振るユニークなID(連番)
-	private String active;// 退会済みであるか否かを判断します(ユーザーからは見えない要素)
+	private String state;// valid(有効)、invalid(無効)、退会済みであるか否かを判断します(ユーザーからは見えない要素)
 	private String mail;// メールアドレス(ログインに用いるため重複不可)
-	private String password;// 8文字以上16文字以下の半角英数字(ハイフンやアンダーバーなどの記号は不可)
+	private String password;// 8文字の半角英数字(ハイフンやアンダーバーなどの記号は不可)
 	private String name;// 名前(日本人以外も登録する可能性があるため制限なし)
 	private String reading;// 全角カナ、読み方
 	private String birthday;// 誕生日(2000-01-01形式)、現在よりも後の日付や閏年以外の2月29日など存在しない日は入力できない
@@ -14,7 +14,84 @@ public class Customer {
 	private String telephone;// 電話番号(未入力可)
 	private String address;// 住所(未入力可)
 
+	public String get_id() {
+		return _id;
+	}
 
+	public void set_id(String _id) {
+		this._id = _id;
+	}
 
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getReading() {
+		return reading;
+	}
+
+	public void setReading(String reading) {
+		this.reading = reading;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
--- src/com/example/logicalchecksystem/MainActivity.java
+++ src/com/example/logicalchecksystem/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.logicalchecksystem;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private PlaceholderFragment placeholderFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		// actionbarのホームアイコンを非表示にします。
+		getActionBar().setDisplayShowHomeEnabled(false);
+		// actionbarのタイトルを非表示にします。
+		getActionBar().setDisplayShowTitleEnabled(false);
+
+		placeholderFragment = new PlaceholderFragment();
+
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment())
+					.add(R.id.container, placeholderFragment)
 					.commit();
 		}
 	}
@@ -28,6 +44,9 @@ public class MainActivity extends Activity {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+
+		menu.findItem(R.id.action_add).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+		menu.findItem(R.id.action_table).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
 		return true;
 	}
 
@@ -37,7 +56,25 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_add) {
+
+			if (placeholderFragment == null) {
+				getFragmentManager().beginTransaction()
+						.replace(R.id.container, new PlaceholderFragment())
+						.commit();
+			} else {
+				getFragmentManager().beginTransaction()
+						.replace(R.id.container, placeholderFragment)
+						.commit();
+			}
+
+			return true;
+		} else if (id == R.id.action_table) {
+
+			getFragmentManager().beginTransaction()
+					.replace(R.id.container, new TableFragment())
+					.commit();
+
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -48,6 +85,9 @@ public class MainActivity extends Activity {
 	 */
 	public static class PlaceholderFragment extends Fragment {
 
+		private TextView birthdayTV;
+		private Dialog datePickerDialog;
+
 		public PlaceholderFragment() {
 		}
 
@@ -55,9 +95,51 @@ public class MainActivity extends Activity {
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+			//日付情報を初期設定します。
+			// Calendar.MONTHは0(JANUARY)から始まります。
+			// http://www.javadrive.jp/start/calendar/index2.html
+
+			Calendar calendar = Calendar.getInstance();
+			int year = calendar.get(Calendar.YEAR);
+			int monthOfYear = calendar.get(Calendar.MONTH);
+			int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+			// SimpleDateFormatクラスによる書式設定
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+			sdf.applyPattern("yyyy'年'MM'月'dd'日'HH':'mm':'ss");
+			final String tmp = sdf.format(Calendar.getInstance().getTime());
+
+			//日付設定時のリスナーを作成します。
+			DatePickerDialog.OnDateSetListener DateSetListener = new DatePickerDialog.OnDateSetListener() {
+				public void onDateSet(android.widget.DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+
+					birthdayTV.setText(year + "年" + monthOfYear + 1 + "月" + dayOfMonth + "日");
+
+				}
+
+			};
+
+			// 日付設定ダイアログを作成します。
+			datePickerDialog = new DatePickerDialog(getActivity(), DateSetListener, year, monthOfYear, dayOfMonth);
+
+			birthdayTV = (TextView) rootView.findViewById(R.id.birthdaySettingTV);
+			birthdayTV.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+
+					// 日付設定ダイアログを表示します。
+					datePickerDialog.show();
+
+				}
+			});
+
 			return rootView;
 		}
 	}
+
 	/**
 	 * 一覧表をリスト表示するフラグメントです。
 	 */
@@ -69,7 +151,7 @@ public class MainActivity extends Activity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+			View rootView = inflater.inflate(R.layout.fragment_table, container, false);
 			return rootView;
 		}
 	}
